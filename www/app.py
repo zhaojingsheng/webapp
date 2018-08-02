dfa29052305fdb5da76d511996db4117b2df8e79
zhaojingsheng <zhjsdyx@163.com>
2018-08-02 15:23:22 +0800

    firt commit

---
+++ www/app.py
@@ -0,0 +1,20 @@
+import logging; logging.basicConfig(level=logging.INFO)
+
+import asyncio, os, json, time,aiomysql
+from datetime import datetime
+from logging import log
+
+from aiohttp import web
+routes=web.RouteTableDef()
+
+@routes.get("/")
+async  def index(request):
+    return web.Response(body=b'<h1>test</h1>')
+
+def init():
+    app=web.Application()
+    app.add_routes(routes)
+    logging.info('server started at http://127.0.0.1:9000...')
+    web.run_app(app,host="127.0.0.1",port=9000)
+
+init()
