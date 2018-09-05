2cdcd01e04e0161f84537e1b0c70ace844a8e178
zhaojingsheng <zhjsdyx@163.com>
2018-09-05 15:40:36 +0800

    first

--- www/app.py
+++ www/app.py
@@ -3,18 +3,22 @@ import logging; logging.basicConfig(level=logging.INFO)
 import asyncio, os, json, time,aiomysql
 from datetime import datetime
 from logging import log
-
+import aiohttp_jinja2
 from aiohttp import web
 routes=web.RouteTableDef()
+import jinja2
 
 @routes.get("/")
-async  def index(request):
-    return web.Response(body=b'<h1>test</h1>',content_type="text/html")
+@aiohttp_jinja2.template("index.html")
+async def index(request):
+    return {}
 
 def init():
     app=web.Application()
+    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader(r".\static"))
     app.add_routes(routes)
     logging.info('server started at http://127.0.0.1:9000...')
-    web.run_app(app,host="127.0.0.1",port=9000)
+    return app
 
-init()
+
+web.run_app(init(),port=9000)
